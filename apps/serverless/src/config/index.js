0b9198d24093883f3a41ea880364b617ddd4110d
andrronov <andrew270504cska@gmail.com>
2026-03-06 05:10:01 +0500

    feat/KC-25: delete all gash js files

--- apps/serverless/src/config/index.js
+++
@@ -1,9 +0,0 @@
-export const CF_MINUTE = 60;
-export const CF_5_MINUTES = CF_MINUTE * 5;
-export const CF_DAY = 60 * 60 * 24;
-export const CF_AUTH_EXP = CF_5_MINUTES;
-export const CF_SESSION_EXP = CF_DAY * 30;
-export const OTP_LENGTH = 6;
-export const SESSION_COOKIE = "session_id";
-export const getKVSessionKey = (sessionId) => `kc-session-${sessionId}`;
-export const CONTEXT_USER = "user";
--- apps/serverless/src/db/index.js
+++
@@ -1,8 +0,0 @@
-import { drizzle } from "drizzle-orm/neon-http";
-import { neon } from "@neondatabase/serverless";
-import * as dbSchema from "@kris-customs/shared/db";
-import * as relations from "../../drizzle/relations";
-import "dotenv/config";
-const sql = neon(process.env.DATABASE_URL);
-const schema = { ...dbSchema, ...relations };
-export const db = drizzle({ client: sql, schema });
