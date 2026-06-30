8ca271c826e1c8bd6b8eef819773ccb51da17f2e
andrronov <andrew270504cska@gmail.com>
2026-07-01 02:03:13 +0700

    chore/KC-29(db): generate & migrate database after removing product-collections

---
+++ apps/serverless/drizzle/0009_greedy_mole_man.sql
@@ -0,0 +1,3 @@
+DROP TABLE "collections" CASCADE;--> statement-breakpoint
+DROP TABLE "product_collections" CASCADE;--> statement-breakpoint
+ALTER TABLE "product_variants" DROP COLUMN "stock_quantity";
